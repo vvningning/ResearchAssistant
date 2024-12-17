4ea238579bbc50d1cba6a67b060d195eddbbf867
LYNN-JEAN <1021700272@qq.com>
2024-12-17 14:44:45 +0800

    MySQL settings

--- RESAsst/RESAsst/settings.py
+++ RESAsst/RESAsst/settings.py
@@ -79,9 +79,14 @@ WSGI_APPLICATION = "RESAsst.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'live',
+        'USER': 'test',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 
