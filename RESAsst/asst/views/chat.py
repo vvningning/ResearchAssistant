e981f0716a75a69ff63d768ed01eff5ff4dd73f0
LYNN-JEAN <1021700272@qq.com>
2024-12-17 19:29:44 +0800

    update views

--- RESAsst/asst/views/chat.py
+++ RESAsst/asst/views/chat.py
@@ -2,6 +2,13 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
+from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.llms.chatglm import ChatGLM
+from langchain_core.messages import ChatMessage
+from langchain.chains import RetrievalQA
+from langchain_community.chat_models import ChatSparkLLM
+
 
 def chat(request):
     response = {}
@@ -9,6 +16,5 @@ def chat(request):
     print(question)
     response['ans'] = "This is an answer."
     return JsonResponse(response)
-
 
 # @csrf_exempt 如果是post加个这个
