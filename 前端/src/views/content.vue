b8bebb54f7d5808ffe17894244998f0cdb6ade8f
姑苏一杯倒 <18913805248@163.com>
2024-12-28 22:28:20 +0800

    Revert "update django"
    This reverts commit d3447caa

--- 前端/src/views/content.vue
+++ 前端/src/views/content.vue
@@ -3,16 +3,31 @@
     <div style="width: 99%; min-width: 1200px; display: inline;">
 	    <el-card style="width: 100%;height: 98%">
 		    <div style="display: flex; width: 100%;height: 100%">
-				    this is the content uses el-card
-		    </div>
+          <iframe
+              :src="fileUrl"
+              id="iframeBox"
+              ref="iframeRef"
+              style="width: 100%; height: 800px"
+          ></iframe>
+
+        </div>
 	    </el-card>
     </div>
 	</div>
 </template>
 
 <script>
+import pdf from "vue-pdf"
 export default {
-    name: "content"
+  name: "content",
+  data() {
+    return {
+      fileUrl: "../pdf/test.pdf", //文件路径
+    };
+  },
+  components:{
+    pdf
+  }
 }
 </script>
 
